5deba960f3b493da7be0556672f8c8f9b88c6b82
Osamu <oskatagiri@gmail.com>
2017-04-02 03:18:32 -0500

    infinite loop until keyboard input

--- p_blink.c
+++ p_blink.c
@@ -14,13 +14,14 @@
 #include <stdint.h>
 #include <string.h>
 #include <errno.h>
- 
+
+#include <ncurses.h>
+
 #include <wiringPi.h>
 #include <wiringPiSPI.h>
  
-#define	TRUE	            (1==1)
-#define	FALSE	            (!TRUE)
 #define CHAN_CONFIG_SINGLE  8
+
  
 static int myFd ;
 
@@ -43,9 +44,10 @@ int myAnalogRead(int spiChannel,int channelConfig,int analogChannel)
     return ( (buffer[1] & 3 ) << 8 ) + buffer[2]; // get last 10 bits
 }
 
+
 int main ()
 {
-    int loadSpi       = FALSE;
+    int loadSpi       = 0; // 0:false; 1:true
     int channelConfig = CHAN_CONFIG_SINGLE;
     int analogChannel = 0;
     int spiChannel    = 0;
@@ -54,9 +56,30 @@ int main ()
     wiringPiSetup () ;
     spiSetup(spiChannel);
     
-    analogRead = myAnalogRead(spiChannel,channelConfig,0);
-    printf("analogRead = %d\n",analogRead);
+    int i=0;
+ 
+    printf("\nProgram START.\n");
+    initscr();     //in ncurses
+    timeout(0);
+    
+/********************/
+/*** Main Program ***/
+    
+    while(!i)
+    {
+        analogRead = myAnalogRead(spiChannel,channelConfig,0);
+        printf("analogRead = %d\n\r",analogRead);
+        i=getch();
+        printw("%d ",i);
+        if (i>0) {i=1;} else {i=0;}
+    }
+    
+/*** Main Program ***/
+/********************/
+    
+    endwin();
+    printf("\nProgram END.\n");
 
-    close (myFd) ;
+    close (myFd);
     return 0;
 }
